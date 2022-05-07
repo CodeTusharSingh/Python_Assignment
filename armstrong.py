0555f84814d7059bd990b0828bdf0d04c8058898
Tushar Singh <99042369+CodeTusharSingh@users.noreply.github.com>
2022-05-08 02:43:38 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,19 @@
+n = int(input("enter a number to check whether it is a armstrong number or not: "))
+i = 0
+sum = 0
+c = n
+if(n >= 1000):
+    while(c != 0):
+        t = c % 10
+        sum = sum + (t*t*t*t)
+        c = c//10
+else:
+    while(c != 0):
+        r = c % 10
+        sum = sum + (r*r*r)
+        c = c//10
+
+if(sum == n):
+    print(n, "is a armstrong number")
+else:
+    print(n, "is not a armstrong number")
